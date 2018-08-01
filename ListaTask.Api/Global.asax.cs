8f55d5bc77644d3f51b6c87b38ec3ca241a32b3b
Rafael-Prado <rafaeldacruzdoprado@hotmail.com>
2018-08-01 09:23:56 -0300

    adicionando injeção de dependencias SimpleInjectorWebApi

--- ListaTask.Api/Global.asax.cs
+++ ListaTask.Api/Global.asax.cs
@@ -1,4 +1,10 @@
-﻿using System.Web.Http;
+﻿using ListaTask.Api.Service;
+using ListaTask.Infra.Repositories;
+using ListTask.Domain.Repository.Interface;
+using SimpleInjector;
+using SimpleInjector.Integration.WebApi;
+using System.Web.Http;
+using System.Web.Http.Dependencies;
 
 namespace ListaTask.Api
 {
@@ -6,7 +12,20 @@ namespace ListaTask.Api
     {
         protected void Application_Start()
         {
-            GlobalConfiguration.Configure(WebApiConfig.Register);
+
+            GlobalConfiguration.Configure(WebApiConfig.Register);        
+
+
+        var container = new Container();
+
+        container.Register<ITaskServece, TaskService>(Lifestyle.Scoped);
+        container.Register<ITaskInterfaceRepository, TaskRepository>(Lifestyle.Scoped);
+ 
+        container.RegisterWebApiControllers(GlobalConfiguration.Configuration); //web api          
+        container.Verify();
+        
+        GlobalConfiguration.Configuration.DependencyResolver = new SimpleInjectorWebApiDependencyResolver(container);
+
         }
     }
 }
